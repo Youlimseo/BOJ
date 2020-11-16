d945cee179cb6af2f763871b1683fafc2502d43f
Youlimseo <72193513+Youlimseo@users.noreply.github.com>
2020-11-16 17:53:57 +0900

    Create 5086_배수와 약수.c

---
+++ 구현/5086_배수와 약수.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+	int a, b;
+	while(1)
+	{
+		scanf("%d %d", &a, &b);
+		if (a == 0 && b == 0)
+			break;
+		else if (a % b == 0 && a > b)
+			printf("multiple\n");
+		else if (b % a == 0 && b > a)
+			printf("factor\n");
+		else
+			printf("neither\n");
+	}
+	return 0;
+}
